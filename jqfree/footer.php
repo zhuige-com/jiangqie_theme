153e8ae2f443f612575266b2a3ce48c3fb803fd0
longwenjunjie <longwenjunj@163.com>
2021-03-13 14:53:29 +0800

    bug fix

--- jqfree/footer.php
+++ jqfree/footer.php
@@ -1,14 +1,17 @@
 <!--页脚-->
 <footer>
 	<!--页脚图标链接-->
-	<div class="container ta-c pb-30 main-footer-links">
-		<?php $footer_icons = jiangqie_option('footer_icons');
-		foreach ($footer_icons as $icon) : ?>
-			<a style="color: white;" href="<?php echo $icon['url'] ?>" title="" target="_blank">
-				<img src="<?php echo $icon['image']['url'] ?>" width="50" height="50" />
-			</a>
-		<?php endforeach; ?>
-	</div>
+	<?php $footer_icons = jiangqie_option('footer_icons');
+	if (is_array($footer_icons) && !empty($footer_icons)) { ?>
+		<div class="container ta-c pb-30 main-footer-links">
+			<?php
+			foreach ($footer_icons as $icon) : ?>
+				<a style="color: white;" href="<?php echo $icon['url'] ?>" title="" target="_blank">
+					<img src="<?php echo $icon['image']['url'] ?>" width="50" height="50" />
+				</a>
+			<?php endforeach; ?>
+		</div>
+	<?php } ?>
 	<div class="container ta-c main-footer">
 		<!--页脚分栏-->
 		<div class="row d-flex flex-wrap">
@@ -34,7 +37,7 @@
 				if ($footer_hot_recommend) :
 					foreach ($footer_hot_recommend as $post) : ?>
 						<p><a href="<?php echo get_permalink($post['id']) ?>" title="<?php echo $post['title'] ?>"><?php echo $post['title'] ?></a></p>
-					<?php endforeach;
+				<?php endforeach;
 				endif;
 				?>
 			</div>
@@ -49,10 +52,14 @@
 <?php wp_footer(); ?>
 
 <div id="toTop">
-    <img src="<?php echo get_stylesheet_directory_uri() . '/images/toTop.png'; ?>">
+	<img src="<?php echo get_stylesheet_directory_uri() . '/images/toTop.png'; ?>">
 </div>
 
-<div style="display: none;"><script><?php echo jiangqie_option('footer_statistics'); ?></script><div>
-</body>
+<div style="display: none;">
+	<script>
+		<?php echo jiangqie_option('footer_statistics'); ?>
+	</script>
+	<div>
+		</body>
 
-</html>
+		</html>
