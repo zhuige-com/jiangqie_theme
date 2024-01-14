bda0e1096089cc8465345a5d310f6f211145acc2
longwenjunjie <longwenjunj@163.com>
2024-01-14 16:17:42 +0800

    fix

--- jqfree/inc/zhuige-market.php
+++ jqfree/inc/zhuige-market.php
@@ -1,14 +1,4 @@
 <?php
-
-/**
- * 追格小程序
- * 作者: 追格
- * 文档: https://www.zhuige.com/docs/zg.html
- * gitee: https://gitee.com/zhuige_com/zhuige_xcx
- * github: https://github.com/zhuige-com/zhuige_xcx
- * Copyright © 2022-2023 www.zhuige.com All rights reserved.
- */
-
 if (!defined('ABSPATH')) {
 	exit;
 }
